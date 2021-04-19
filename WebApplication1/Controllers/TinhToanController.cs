1dff234688b4ed4294f4036d2934e488ca8eec58
tranbaokhanh <tranbaokhanhns@gmail.com>
2021-04-19 10:48:58 +0700

    khởi tạo project VS

---
+++ WebApplication1/Controllers/TinhToanController.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace WebApplication1.Controllers
+{
+    public class TinhToanController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
